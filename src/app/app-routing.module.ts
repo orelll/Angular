a37a6a5e2c5b027310272e93e437776d4e1ad35b
Szymon Orlik <szymon.orlik@capgemini.com>
2020-04-17 20:09:31 +0200

    koniec lekcji

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ToDoDetailsComponent } from './to-do-details/to-do-details.component';
 
-
+const routes: Routes = [
+  { path: '',  redirectTo: '/dashboard', pathMatch: 'full'},
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'todo/:id', component: ToDoDetailsComponent}
+];
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
