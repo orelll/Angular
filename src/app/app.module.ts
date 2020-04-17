4e217a2d3cc254118552f75c05438bebf05dd640
Szymon Orlik <szymon.orlik@capgemini.com>
2020-04-17 14:34:53 +0200

    prepared for lesson

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ToDoDetailsComponent } from './to-do-details/to-do-details.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ToDoesComponent } from './to-does/to-does.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ToDoDetailsComponent,
-    DashboardComponent,
-    ToDoesComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
