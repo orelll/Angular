f8378559b2ea72c7455e8675ed924cb9d3ecca9f
Szymon Orlik <szymon.orlik@capgemini.com>
2020-05-15 17:21:46 +0200

    przygotowana lekcja

--- src/app/to-do-details/to-do-details.component.ts
+++ src/app/to-do-details/to-do-details.component.ts
@@ -6,23 +6,22 @@ import { ToDo } from '../common/todo';
 @Component({
   selector: 'app-to-do-details',
   templateUrl: './to-do-details.component.html',
-  styleUrls: ['./to-do-details.component.css']
+  styleUrls: ['./to-do-details.component.css'],
 })
 export class ToDoDetailsComponent implements OnInit {
-
   todo: ToDo;
 
-  constructor(private todoesService: TodoesServiceService,
-    private route: ActivatedRoute) { 
-      this.getToDoDetails();
-    }
-
-  ngOnInit(): void {
+  constructor(
+    private todoesService: TodoesServiceService,
+    private route: ActivatedRoute
+  ) {
+    this.getToDoDetails();
   }
 
-getToDoDetails(){
-  const id = +this.route.snapshot.paramMap.get('id');
-  this.todo = this.todoesService.getToDo(id);
-}
+  ngOnInit(): void {}
 
+  getToDoDetails() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.todo = this.todoesService.getToDo(id);
+  }
 }
